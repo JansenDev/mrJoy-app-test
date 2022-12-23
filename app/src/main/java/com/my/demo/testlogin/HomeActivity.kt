f8298b1b0fe1d099b62d40a5b90484a89d195363
JHONATAN JANSSEN SEGURA GALINDO <jsegura-e@ripley.com.pe>
2022-12-22 23:13:51 -0500

    added: decoration o separador entre items en RecyclerView

--- app/src/main/java/com/my/demo/testlogin/HomeActivity.kt
+++ app/src/main/java/com/my/demo/testlogin/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.my.demo.testlogin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.my.demo.testlogin.adapter.SuperHeroAdapter
@@ -22,10 +23,14 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun initRecyclerView() {
-        binding.rvSuperHero.layoutManager = LinearLayoutManager(this)
+        val manager = LinearLayoutManager(this)
+        val decoration = DividerItemDecoration(this, manager.orientation)
+
+        binding.rvSuperHero.layoutManager = manager
         binding.rvSuperHero.adapter = SuperHeroAdapter(superHeroList) { superHero ->
             onSelectedSuperHero(superHero)
         };
+        binding.rvSuperHero.addItemDecoration(decoration)
     }
 
     private fun onSelectedSuperHero(superHero: SuperHero) {
